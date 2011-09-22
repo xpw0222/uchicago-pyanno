cc35b570dc46672a8262f9a6adc49888cd1cb583
Pietro Berkes <pberkes@enthought.com>
2011-09-22 16:00:56 +0100

    TST: testing log likelihood in ModelBt

--- pyanno/test/test_modelBt.py
+++ pyanno/test/test_modelBt.py
@@ -8,20 +8,50 @@ class TestModelB(unittest.TestCase):
 
     def test_mle_estimation(self):
         # test simple model, check that we get to global optimum
-        nclasses, nitems = 3, 5000*8
-        # create random model and data (this is our graund truth model)
+        nclasses, nitems = 3, 500*8
+        # create random model and data (this is our ground truth model)
         true_model = ModelBt.random_model(nclasses, nitems)
         labels = true_model.generate_labels()
         annotations = true_model.generate_annotations(labels)
 
         # create a new, empty model and infer back the parameters
         model = ModelBt.random_model(nclasses, nitems)
+        before_llhood = model.log_likelihood(annotations)
         model.mle(annotations)
+        after_llhood = model.log_likelihood(annotations)
 
-        testing.assert_allclose(model.gamma, true_model.gamma, atol=1e-2, rtol=0.)
-        testing.assert_allclose(model.theta, true_model.theta, atol=1e-2, rtol=0.)
+        self.assertGreater(after_llhood, before_llhood)
+        testing.assert_allclose(model.gamma, true_model.gamma, atol=1e-1, rtol=0.)
+        testing.assert_allclose(model.theta, true_model.theta, atol=1e-1, rtol=0.)
 
+
+    def test_log_likelihood(self):
+        # check that log likelihood is maximal at true parameters
+        nclasses, nitems = 3, 1500*8
+        # create random model and data (this is our ground truth model)
+        true_model = ModelBt.random_model(nclasses, nitems)
+        labels = true_model.generate_labels()
+        annotations = true_model.generate_annotations(labels)
+
+        max_llhood = true_model.log_likelihood(annotations)
+        # perturb gamma
+        for _ in xrange(20):
+            theta = true_model.theta
+            gamma = sp.random.normal(loc=true_model.gamma, scale=0.1)
+            gamma = sp.clip(gamma, 0., 1.)
+            gamma /= gamma.sum()
+            model = ModelBt(nclasses, nitems, gamma, theta)
+            llhood = model.log_likelihood(annotations)
+            self.assertGreater(max_llhood, llhood)
+
+        # perturb theta
+        for _ in xrange(20):
+            gamma = true_model.gamma
+            theta = sp.random.normal(loc=true_model.theta, scale=0.1)
+            theta = sp.clip(theta, 0., 1.)
+            model = ModelBt(nclasses, nitems, gamma, theta)
+            llhood = model.log_likelihood(annotations)
+            self.assertGreater(max_llhood, llhood)
 
 if __name__ == '__main__':
     unittest.main()
-
