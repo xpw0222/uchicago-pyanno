258a12f33e38c5b5064e6bd8987c744afa0b3b16
Pietro Berkes <pberkes@enthought.com>
2011-09-26 10:26:45 +0100

    RF: clean up test for sampling

--- pyanno/test/test_sampling.py
+++ pyanno/test/test_sampling.py
@@ -6,12 +6,21 @@ from pyanno.modelAB import sample_distribution, optimum_jump
 from pyanno.util import log_beta_pdf
 
 class TestSampling(unittest.TestCase):
-    def test_samples_distribution(self):
-        # check sample_distribution's ability to sample from a beta distribution
+
+    def test_sample_distribution(self):
+        # check sample_distribution's ability to sample from a
+        # fixed beta distribution
         nclasses = 8
         nitems = 1000
+        # pick random parameters 
         a = np.random.uniform(1., 5., size=(nclasses,))
         b = np.random.uniform(4., 6., size=(nclasses,))
+
+        # sample values from a beta distribution with fixed parameters
+        values = np.empty((nitems, nclasses))
+        for k in range(nclasses):
+            values[:,k] = scipy.stats.beta.rvs(a[k], b[k], size=nitems)
+        arguments = values
 
         def beta_likelihood(params, values):
             a = params[:nclasses].copy()
@@ -20,11 +29,6 @@ class TestSampling(unittest.TestCase):
             for k in range(nclasses):
                 llhood += log_beta_pdf(values[:,k], a[k], b[k]).sum()
             return -llhood
-
-        values = np.empty((nitems, nclasses))
-        for k in range(nclasses):
-            values[:,k] = scipy.stats.beta.rvs(a[k], b[k], size=nitems)
-        arguments = values
 
         x_lower = np.zeros((nclasses*2,)) + 0.5
         x_upper = np.zeros((nclasses*2,)) + 8.
@@ -37,10 +41,11 @@ class TestSampling(unittest.TestCase):
         njumps = 3000
         samples = sample_distribution(beta_likelihood, x0.copy(), arguments,
                                       dx, njumps, x_lower, x_upper, 'Everything')
+        samples = samples[100:]
 
         z = np.absolute((samples.mean(0) - np.r_[a,b]) / samples.std(0))
+        testing.assert_array_less(z, 3.)
 
-        np.testing.assert_array_less(z, 3.)
 
 if __name__ == '__main__':
     unittest.main()
