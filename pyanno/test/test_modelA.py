c422bdfc14ec2efbc1db605931e37a3768b140fd
Pietro Berkes <pberkes@enthought.com>
2011-09-28 14:49:28 +0100

    TST: new tests for ModelA log likelihood

--- pyanno/test/test_modelA.py
+++ pyanno/test/test_modelA.py
@@ -94,11 +94,11 @@ class TestModelA(unittest.TestCase):
 
         # create a new, empty model and infer back the parameters
         model = ModelA.random_model(nclasses, omega=true_model.omega)
-        #before_llhood = model.log_likelihood(annotations)
+        before_llhood = model.log_likelihood(annotations)
         model.mle(annotations, estimate_alpha=False, estimate_omega=False)
-        #after_llhood = model.log_likelihood(annotations)
+        after_llhood = model.log_likelihood(annotations)
 
-        #self.assertGreater(after_llhood, before_llhood)
+        self.assertGreater(after_llhood, before_llhood)
         testing.assert_allclose(model.theta, true_model.theta, atol=1e-1, rtol=0.)
 
 
@@ -106,6 +106,33 @@ class TestModelA(unittest.TestCase):
         pass
         #testing.assert_allclose(model.alpha, true_model.alpha, atol=1e-1, rtol=0.)
 
+
+    def test_log_likelihood(self):
+        # check that log likelihood is maximal at true parameters
+        nclasses, nitems = 3, 1500*8
+        # create random model and data (this is our ground truth model)
+        true_model = ModelA.random_model(nclasses)
+        annotations = true_model.generate_annotations(nitems)
+
+        max_llhood = true_model.log_likelihood(annotations)
+        # perturb omega
+        for _ in xrange(20):
+            theta = true_model.theta
+            omega = np.random.normal(loc=true_model.omega, scale=0.1)
+            omega = np.clip(omega, 0., 1.)
+            omega /= omega.sum()
+            model = ModelA(nclasses, omega=omega, theta=theta)
+            llhood = model.log_likelihood(annotations)
+            self.assertGreater(max_llhood, llhood)
+
+        # perturb theta
+        for _ in xrange(20):
+            omega = true_model.omega
+            theta = np.random.normal(loc=true_model.theta, scale=0.1)
+            theta = np.clip(theta, 0., 1.)
+            model = ModelA(nclasses, omega=omega, theta=theta)
+            llhood = model.log_likelihood(annotations)
+            self.assertGreater(max_llhood, llhood)
 
 
 if __name__ == '__main__':
