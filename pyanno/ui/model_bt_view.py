57f288cb3adc1e11dcd60c994278c05186f7db52
Pietro Berkes <pberkes@enthought.com>
2011-10-05 10:10:22 +0100

    NEW: make gamma editable

--- pyanno/ui/model_bt_view.py
+++ pyanno/ui/model_bt_view.py
@@ -1,12 +1,21 @@
+from chaco.data_label import DataLabel
 from chaco.data_range_2d import DataRange2D
+from chaco.text_box_overlay import TextBoxOverlay
+from chaco.tooltip import ToolTip
+from traits.trait_numeric import Array
+from traits.trait_types import Button, Range, Int
 from traitsui.api import ModelView, View, Item, Group, Tabbed, VGroup, HGroup
-from traitsui.menu import OKButton
+from traitsui.editors.array_editor import ArrayEditor
+from traitsui.editors.instance_editor import InstanceEditor
+from traitsui.item import Heading
+from traitsui.menu import OKButton, OKCancelButtons
 from traits.api import Instance
 from enable.component_editor import ComponentEditor
 from chaco.api import ArrayPlotData, Plot
 from chaco.base import n_gon
 import numpy as np
 
+from pyanno.ui.arrayview import create_array2d_editor
 
 def _hinton_diagram(distr, title=None):
 
@@ -45,19 +54,46 @@ def _hinton_diagram(distr, title=None):
     polyplot.x_axis.tick_interval = 1.
     polyplot.y_axis.tick_interval = 1.
     #polyplot.y_axis.tick_visible = False
+    polyplot.border_visible = False
+    #polyplot.x_axis.visible = False
+    polyplot.y_axis.visible = False
+
+    #label = TextBoxOverlay(text='test', border_color='white')
+    #polyplot.overlays.append(label)
 
     return polyplot
 
 
-class ModelBtView(ModelView):
-    """ Traits UI Model/View for the basic properties of 'Formation' objects.
+def create_gamma_view(gamma):
+    width = len(gamma)
+    class GammaView(ModelView):
+        data = Array(dtype=float, shape=(1, width))
+        def _data_default(self):
+            return self.model.gamma[np.newaxis,:]
+        traits_view = View(
+            Group(
+                Item('data',
+                     editor=create_array2d_editor(width, show_index=False),
+                     show_label=False),
+                ),
+            title     = 'Model B-with-Theta, gamma parameters',
+            width     = 500,
+            height    = 200,
+            resizable = True,
+            buttons   = OKCancelButtons
+       )
+    return GammaView
 
+
+class ModelBtView(ModelView):
+    """ Traits UI Model/View for 'ModelBt' objects.
     """
 
     #### Model properties #######
     pass
 
     #### Traits UI view #########
+    gamma_button = Button(label='Edit...')
     gamma_plot = Instance(Plot)
 
     # TODO check that gamma is a distribution, and that bounds are btw 0 and 1
@@ -73,8 +109,14 @@ class ModelBtView(ModelView):
             Item('gamma_plot',
                  editor=ComponentEditor(width=300, height=150),
                  resizable=False,
-                 show_label=False),
-            Item('model.gamma')
+                 show_label=False
+            ),
+            Item('gamma_button', show_label=False),
+            show_border=True
+        ),
+        Item(
+            'model.theta',
+            editor=InstanceEditor(editable=True)
         ),
         Item('model.theta', label="Theta[j] = P(annotation_j=k | label=k)")
     )
@@ -84,12 +126,20 @@ class ModelBtView(ModelView):
     def _gamma_plot_default(self):
         return _hinton_diagram(self.model.gamma)
 
+    def _gamma_button_fired(self):
+        gamma_view = create_gamma_view(self.model.gamma)
+        gamma_view = gamma_view(model=self.model)
+        gamma_view.edit_traits(kind='livemodal')
+
+
+#### Testing and debugging ####################################################
+
 def main():
     """ Entry point for standalone testing/debugging. """
 
     from pyanno.modelBt import ModelBt
 
-    model      = ModelBt.random_model(5, 100)
+    model = ModelBt.create_initial_state(5)
     model_view = ModelBtView(model=model)
     model_view.configure_traits(view='traits_view')
 
