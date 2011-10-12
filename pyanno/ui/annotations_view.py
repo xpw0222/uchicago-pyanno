1c62dddc35f6e4dd485b27e0347e78f4e9b94e92
Pietro Berkes <pberkes@enthought.com>
2011-10-12 11:54:26 +0100

    NEW: view for annotations in main window

--- pyanno/ui/annotations_view.py
+++ pyanno/ui/annotations_view.py
@@ -1,26 +1,56 @@
-from traits.api import ModelView
-from traitsui.api import View
+from traits.has_traits import HasTraits, HasStrictTraits
+from traits.trait_numeric import Array
+from traits.trait_types import Instance, Int, List, ListFloat
+from traits.traits import Property
+from traitsui.api import ModelView, View, VGroup
+from traitsui.item import Item
+from pyanno.ui.hinton_plot import HintonDiagramPlot
+from pyanno.util import annotations_frequency
 
 
-class AnnotationsViewView(ModelView):
-    """ Traits UI Model/View for  'AnnotationsView' objects.
-    """
+class AnnotationsView(HasStrictTraits):
+    """ Traits UI Model/View for annotations."""
 
-    pass
+    ### Model-related traits ###
+    annotations = Array
+    nclasses = Int
 
-    pass
+    frequency = Property(ListFloat, depends_on='annotations,nclasses')
 
+    def _get_frequency(self):
+        return annotations_frequency(self.annotations, self.nclasses).tolist()
+
+    ### Traits UI definitions ###
+    frequency_plot = Instance(HintonDiagramPlot)
+
+    def _frequency_plot_default(self):
+        return HintonDiagramPlot(data = self.frequency)
+
+    ### View definition ###
+    body = VGroup(
+        Item('frequency_plot',
+             style='custom',
+             resizable=False,
+             show_label=False
+        )
+    )
+
+    traits_view = View(body)
+
+
+
+#### Testing and debugging ####################################################
 
 def main():
     """ Entry point for standalone testing/debugging. """
 
-    from a import AnnotationsView
-
-
-    model = AnnotationsView.random_model(5, 100)
-    model_view = ModelView(model=model)
-    model_view.configure_traits(view='debug_view')
+    from pyanno.modelBt import ModelBt
+    model = ModelBt.create_initial_state(5)
+    annotations = model.generate_annotations(model.generate_labels(100))
+    model_view = AnnotationsView(annotations=annotations, nclasses=5)
+    model_view.configure_traits()
+    return model, annotations, model_view
 
 
 if __name__ == '__main__':
-    main()
+    m, a, mv = main()
