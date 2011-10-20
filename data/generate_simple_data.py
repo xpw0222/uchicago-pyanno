deec47b7ae228499d3139b9cacaa3ba284eff1c2
Pietro Berkes <pberkes@enthought.com>
2011-10-20 16:37:18 +0100

    NEW: generate text annotations

--- data/generate_simple_data.py
+++ data/generate_simple_data.py
@@ -6,13 +6,21 @@ DATA_PER_LOOP = 100
 # TODO replace ModelB with ModelBt and specify target annotator accuracy
 
 # create simple model for 8 annotators
-model = ModelB.create_initial_state(nclasses=4, nannotators=8, nitems=8*DATA_PER_LOOP)
+model = ModelB.create_initial_state(nclasses=3, nannotators=8)
 # generate data
-labels = model.generate_labels()
-data = model.generate_annotations(labels).T + 1
+labels = model.generate_labels(8*DATA_PER_LOOP)
+data = model.generate_annotations(labels) + 1
 # mask data according to "loop" experimental design
 for l in xrange(8):
     label_idx = sp.arange(l+3, l+8) % 8
     data[l*DATA_PER_LOOP:(l+1)*DATA_PER_LOOP, label_idx] = -1
+
+int2str = {1: 'LOW', 2: 'MEDIUM', 3: 'HIGH', -1: 'NA'}
+anno = []
+for i in range(data.shape[0]):
+    anno.append([])
+    for j in range(data.shape[1]):
+        anno[-1].append(int2str[data[i,j]])
+
 # save data
-sp.savetxt('testdata.txt', data, fmt='%d')
+sp.savetxt('testdata_words.txt', anno, fmt='%s')
