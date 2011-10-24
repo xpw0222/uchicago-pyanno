e5f686025c1bf1074e71c8aad8e55217e2be73bc
Pietro Berkes <pberkes@enthought.com>
2011-10-24 15:38:20 +0100

    NEW: Save tool opens file dialog and asks for file

--- pyanno/plots/plot_tools.py
+++ pyanno/plots/plot_tools.py
@@ -5,17 +5,21 @@
 """Plot tools to save plots and copy their data inside to the clipboard.
 """
 
-# Major library imports
-import os.path
-
-# Enthought library imports
 from chaco.plot_graphics_context import PlotGraphicsContext
 from chaco.tools.save_tool import SaveTool
 from enable.base_tool import BaseTool
-from traits.trait_types import Int, Any
+from traits.has_traits import HasTraits
+from traits.trait_types import Int, Any, File
 from pyface.api import clipboard
 from enthought.etsconfig.api import ETSConfig
 
+from traitsui.editors.file_editor import FileEditor
+from traitsui.group import VGroup
+from traitsui.item import Item
+from traitsui.menu import OKCancelButtons
+from traitsui.view import View
+
+import os.path
 import numpy as np
 
 
@@ -38,37 +42,60 @@ def _is_control_down(key_event):
     return is_control_down
 
 
+class SaveFileDialog(HasTraits):
+    save_file = File(exists=False)
+    dpi = Int(300)
+
+    traits_view = View(
+        VGroup(
+            Item('save_file', label='Save to:',
+                 editor=FileEditor(allow_dir=False,
+                                   dialog_style='save',
+                                   entries=0),
+                 style='simple'),
+            Item('dpi', label='Resolution (dpi):')
+        ),
+        title = 'Save plot',
+        width = 400,
+        resizable=True,
+        buttons=OKCancelButtons
+    )
+
+
 class SaveToolPlus(SaveTool):
     """Subclass of SaveTool that requests a filename and dpi before saving."""
 
-    dpi = Int(300)
+    def normal_key_released(self, event):
+        """Handles a key-release when the tool is in the 'normal' state.
 
-    def normal_key_pressed(self, event):
-        """ Handles a key-press when the tool is in the 'normal' state.
-
-        Saves an image of the plot if the keys pressed are Control and S.
+        Saves an image of the plot when the user presses Ctrl-S.
         """
 
         if self.component is None: return
 
         if ((event.character == "s" or event.character == u'\x13')
             and _is_control_down(event)):
-            # TODO: request name of file, dpi
-            print 'triggered!!'
 
-            if os.path.splitext(self.filename)[-1] == ".pdf":
-                self._save_pdf()
-            else:
-                self._save_raster()
+            dialog = SaveFileDialog()
+            dialog.edit_traits(kind='modal')
+
+            self.filename = dialog.save_file
+
+            if self.filename != '':
+                if os.path.splitext(self.filename)[-1] == ".pdf":
+                    self._save_pdf()
+                else:
+                    self._save_raster(dpi=dialog.dpi)
+
             event.handled = True
 
-    def _save_raster(self):
+    def _save_raster(self, dpi=300):
         """ Saves an image of the component."""
         self.component.do_layout(force=True)
         # NOTE saving only works properly when dpi is a multiple of 72
         gc = PlotGraphicsContext((int(self.component.outer_width),
                                   int(self.component.outer_height)),
-                                 dpi=np.ceil(self.dpi / 72.0)*72)
+                                 dpi=np.ceil(dpi / 72.0)*72)
         gc.render_component(self.component)
         gc.save(self.filename)
 
@@ -78,10 +105,10 @@ class CopyDataToClipboardTool(BaseTool):
 
     data = Any
 
-    def normal_key_pressed(self, event):
-        """ Handles a key-press when the tool is in the 'normal' state.
+    def normal_key_released(self, event):
+        """Handles a key-release when the tool is in the 'normal' state.
 
-        Saves an image of the plot if the keys pressed are Control and S.
+        Copy `data` to the keyboard when the user presses Ctrl-C.
         """
 
         if self.component is None: return
