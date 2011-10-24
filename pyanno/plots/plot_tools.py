c171de256fa9380310d347dd287c13f6ed56e0cb
Pietro Berkes <pberkes@enthought.com>
2011-10-24 10:38:49 +0100

    RF: create new 'plots' sub-package and move plot_tools there

--- pyanno/plots/plot_tools.py
+++ pyanno/plots/plot_tools.py
@@ -9,11 +9,13 @@
 import os.path
 
 # Enthought library imports
+from chaco.plot_graphics_context import PlotGraphicsContext
 from chaco.tools.save_tool import SaveTool
 from enable.base_tool import BaseTool
 from traits.trait_types import Int, Any
 from pyface.api import clipboard
 
+import numpy as np
 
 class SaveToolPlus(SaveTool):
     """Subclass of SaveTool that requests a filename and dpi before saving."""
@@ -40,21 +42,20 @@ class SaveToolPlus(SaveTool):
             event.handled = True
 
     def _save_raster(self):
-        """ Saves an image of the component.
-        """
-        from chaco.api import PlotGraphicsContext
+        """ Saves an image of the component."""
         self.component.do_layout(force=True)
+        # NOTE saving only works properly when dpi is a multiple of 72
         gc = PlotGraphicsContext((int(self.component.outer_width),
                                   int(self.component.outer_height)),
-                                 dpi=(self.dpi // 72)*72)
+                                 dpi=np.ceil(self.dpi / 72.0)*72)
         gc.render_component(self.component)
         gc.save(self.filename)
 
 
 class CopyDataToClipboardTool(BaseTool):
-    """Subclass of SaveTool that requests a filename and dpi before saving."""
+    """Tool that copies the plot's data to the clipboard."""
 
-    # TODO: test w/ image data, files
+    # TODO allow apple-c key combination
 
     data = Any
 
