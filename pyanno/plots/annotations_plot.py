02f94d0ffcb089f0d734daed46b5c8afdbefa0d9
Pietro Berkes <pberkes@enthought.com>
2011-10-31 11:05:34 +0000

    NEW: axis for annotations_plot

--- pyanno/plots/annotations_plot.py
+++ pyanno/plots/annotations_plot.py
@@ -5,7 +5,10 @@ from chaco.array_plot_data import ArrayPlotData
 from chaco.cmap_image_plot import CMapImagePlot
 from chaco.data_range_1d import DataRange1D
 from chaco.default_colormaps import jet, YlOrRd, Reds, BuPu
+from chaco.label_axis import LabelAxis
 from chaco.plot import Plot
+from chaco.scales.scales import FixedScale
+from chaco.scales_tick_generator import ScalesTickGenerator
 from enable.component_editor import ComponentEditor
 
 from tables.array import Array
@@ -16,6 +19,7 @@ from traitsui.item import Item
 from traitsui.view import View
 
 from pyanno.plots.plots_superclass import PyannoPlotContainer
+import numpy as np
 
 
 class PosteriorPlot(PyannoPlotContainer):
@@ -31,7 +35,7 @@ class PosteriorPlot(PyannoPlotContainer):
 
     origin = Str('top left')
 
-    posterior_plot = Instance(CMapImagePlot)
+    posterior_plot = Instance(Plot)
 
     #matrix_plot_container = Instance(HPlotContainer)
 
@@ -48,8 +52,43 @@ class PosteriorPlot(PyannoPlotContainer):
         return colormap
 
 
+    def _create_increment_one_axis(self, plot, start, number, orientation,
+                                   ticks=None):
+        """Create axis with ticks at a distance of one units.
+
+        Parameters
+        ----------
+        plot : Plot
+            plot where the axis will be attached
+        start : float
+            position of first tick
+        number : int
+            number of ticks
+        orientation: ['top', 'bottom', 'left', 'right']
+            position of axis on the plot
+        ticks : list of strings
+            string to be displayed for each tick
+        """
+
+        ids = start + np.arange(0, number)
+        if ticks is None:
+            ticks = [str(idx) for idx in np.arange(0, number)]
+
+        axis = LabelAxis(
+            plot,
+            orientation = orientation,
+            positions = ids,
+            labels = ticks,
+            label_rotation = 0
+        )
+
+        # use a FixedScale tick generator with a resolution of 1
+        axis.tick_generator = ScalesTickGenerator(scale=FixedScale(1.))
+
+        return axis
+
+
     def _posterior_plot_default(self):
-        print 'create plot'
         data = self.posterior
         nannotations, nclasses = data.shape
 
@@ -62,16 +101,29 @@ class PosteriorPlot(PyannoPlotContainer):
 
         img_plot = plot.img_plot("values",
                                  interpolation='nearest',
-
                                  xbounds=(0, nclasses),
                                  ybounds=(0, nannotations),
                                  colormap=self._create_colormap())[0]
 
-        img_plot.aspect_ratio = float(nclasses) / nannotations
-        self.plot_height = int(self.plot_width / img_plot.aspect_ratio)
+        self._remove_grid_and_axes(plot)
 
-        self.decorate_plot(img_plot, self.posterior)
-        return img_plot
+        # create x axis for labels
+        label_axis = self._create_increment_one_axis(plot, 0.5, nclasses, 'top')
+        plot.index_axis = label_axis
+        plot.underlays.append(label_axis)
+
+        # create y axis for values
+        value_axis_ticks = [str(id) for id in range(nannotations-1, -1, -1)]
+        value_axis = self._create_increment_one_axis(plot, 0.5, nannotations,
+                                                     'left', value_axis_ticks)
+        plot.value_axis = value_axis
+        plot.underlays.append(value_axis)
+
+        plot.aspect_ratio = float(nclasses) / nannotations
+        self.plot_height = int(self.plot_width / plot.aspect_ratio)
+
+        self.decorate_plot(plot, self.posterior)
+        return plot
 
 
     def add_markings(self, classes):
@@ -102,8 +154,8 @@ class PosteriorPlot(PyannoPlotContainer):
                 Include('instructions_group'),
                 resizable_plot_item,
             ),
-            height = 300,
             width = 450,
+            height = 800,
             scrollable = True,
             resizable = True
         )
@@ -120,6 +172,7 @@ def main():
 
     matrix = np.random.random(size=(200, 5))
     matrix = matrix / matrix.sum(1)[:,None]
+    matrix[0,0] = 1.
 
     matrix_view = PosteriorPlot(posterior=matrix, title='Debug plot_posterior')
     resizable_view = matrix_view._create_resizable_view()
