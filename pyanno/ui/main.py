d61aa74a64030043a32df2050f23642b258ef656
Pietro Berkes <pberkes@enthought.com>
2011-11-02 15:52:37 +0000

    NEW: setup logging when starting main application window

--- pyanno/ui/main.py
+++ pyanno/ui/main.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2011, Enthought, Ltd.
 # Author: Pietro Berkes <pberkes@enthought.com>
-# License: Modified BSD license (3-clause)
+# License: Apache license
 
 """Entry point for pyanno UI application."""
 
@@ -11,13 +11,26 @@ from pyanno.modelBt import ModelBt
 from pyanno.ui.model_bt_view import ModelBtView
 from pyanno.ui.model_data_view import ModelDataView
 
+import logging
+
+
+def setup_logging():
+    logging.basicConfig(level=logging.DEBUG)
+
 
 def main():
     """Create and start the application."""
 
+    setup_logging()
+
+    # create initial model
     model = ModelBt.create_initial_state(5)
+
+    # create main window
     model_data_view = ModelDataView(model=model,
                                     model_view=ModelBtView(model=model))
+
+    # display main window
     model_data_view.configure_traits()
 
 
