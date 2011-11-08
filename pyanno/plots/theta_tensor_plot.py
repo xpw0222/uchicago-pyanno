1029763920be6cef26e9dcf61be60f8e106e0a16
Pietro Berkes <pberkes@enthought.com>
2011-11-08 14:15:12 +0000

    NEW+RF: move theta tensor plot to plots, add choice bewtween matrix and line plots

---
+++ pyanno/plots/theta_tensor_plot.py
@@ -0,0 +1,206 @@
+# Copyright (c) 2011, Enthought, Ltd.
+# Author: Pietro Berkes <pberkes@enthought.com>
+# License: Modified BSD license (2-clause)
+
+"""TraitsUI view of the Theta tensor in Model B, and its samples."""
+
+from chaco.array_plot_data import ArrayPlotData
+from chaco.data_range_2d import DataRange2D
+from chaco.legend import Legend
+from chaco.plot import Plot
+from chaco.plot_containers import HPlotContainer, VPlotContainer
+from chaco.scales.scales import FixedScale
+from chaco.scales_tick_generator import ScalesTickGenerator
+from chaco.tools.legend_tool import LegendTool
+from chaco.default_colors import palette11 as COLOR_PALETTE
+
+from enable.component_editor import ComponentEditor
+
+from traits.trait_numeric import Array
+from traits.trait_types import Instance, Str, Range, Button, Int, Any
+from traitsui.item import Item
+
+from pyanno.plots.plots_superclass import PyannoPlotContainer
+
+
+import numpy as np
+
+
+def sigmoid(x):
+    return 1./(1.+np.exp(-x))
+
+
+class ThetaTensorPlot(PyannoPlotContainer):
+
+    # reference to the theta tensor for one annotator
+    theta = Array
+
+    # reference to an array of samples for theta for one annotator
+    theta_samples = Any
+
+    # index of the annotator
+    annotator_idx = Int
+
+    # chaco plot of the tensor
+    theta_plot = Any
+
+
+    def _label_name(self, k):
+        """Return a name for the data with index `k`."""
+        return 'theta[{},{},:]'.format(self.annotator_idx,k)
+
+
+    def _plot_samples(self, plot, plot_data):
+        nclasses = self.theta.shape[0]
+        nsamples = self.theta_samples.shape[0]
+
+        for k in range(nclasses):
+            samples = np.sort(self.theta_samples[:,k,:], axis=0)
+            perc5 = samples[int(nsamples*0.05),:]
+            perc95 = samples[int(nsamples*0.95),:]
+            avg = samples.mean(0)
+
+            # build polygon
+            index_name = self._label_name(k) + '_confint_index'
+            value_name = self._label_name(k) + '_confint_value'
+            index_coord = []
+            value_coord = []
+            # bottom part
+            for i in range(nclasses):
+                index_coord.append(i)
+                value_coord.append(perc5[i])
+            # top part
+            for i in range(nclasses-1, -1, -1):
+                index_coord.append(i)
+                value_coord.append(perc95[i])
+
+            plot_data.set_data(index_name, np.array(index_coord,
+                                                    dtype=float))
+            plot_data.set_data(value_name, np.array(value_coord,
+                                                    dtype=float))
+
+            # make color lighter and more transparent
+            color = list(COLOR_PALETTE[k % len(COLOR_PALETTE)])
+            for i in range(3):
+                color[i] = min(1.0, sigmoid(color[i]*5.))
+            color[-1] = 0.3
+
+            plot.plot(
+                (index_name, value_name),
+                type = 'polygon',
+                face_color = color,
+                edge_color = 'black',
+                edge_width = 0.5
+            )
+
+            # add average
+            avg_name = self._label_name(k) + '_avg_value'
+            plot_data.set_data(avg_name, avg)
+            plot.plot(
+                ('classes', avg_name),
+                color = COLOR_PALETTE[k % len(COLOR_PALETTE)],
+                line_style = 'dash'
+            )
+
+
+    def _plot_theta_values(self, plot, plot_data):
+        theta = self.theta
+        nclasses = theta.shape[0]
+
+        data_names = ['classes']
+        for k in range(nclasses):
+            name = self._label_name(k)
+            plot_data.set_data(name, theta[k,:])
+            data_names.append(name)
+
+        plots = {}
+        for k in range(nclasses):
+            name = self._label_name(k)
+            line_plot = plot.plot(
+                ['classes', name],
+                line_width=2.,
+                color=COLOR_PALETTE[k % len(COLOR_PALETTE)],
+                name=name
+            )
+            plots[name] = line_plot
+
+        return plots
+
+
+    def _theta_plot_default(self):
+
+        theta = self.theta
+        nclasses = theta.shape[0]
+
+        # create a plot data object and give it this data
+        plot_data = ArrayPlotData()
+
+        plot_data.set_data('classes', range(nclasses))
+
+        # create the plot
+        plot = Plot(plot_data, fill_padding=True)
+
+        # --- plot theta samples
+        if self.theta_samples is not None:
+            self._plot_samples(plot, plot_data)
+
+        # --- plot values of theta
+        plots = self._plot_theta_values(plot, plot_data)
+
+        # --- adjust plot appearance
+
+        plot.aspect_ratio = 1.7
+
+        # adjust axis bounds
+        y_high = theta.max()
+        if self.theta_samples is not None:
+            y_high = max(y_high, self.theta_samples.max())
+
+        plot.range2d = DataRange2D(
+            low  = (-0.2, 0.0),
+            high = (nclasses-1+0.2, y_high*1.1)
+        )
+
+        # create new horizontal axis
+        label_axis = self._create_increment_one_axis(
+            plot, 0., nclasses, 'bottom')
+        label_axis.title = 'True classes'
+        self._add_index_axis(plot, label_axis)
+
+        # label vertical axis
+        plot.value_axis.title = 'Probability'
+
+        # add legend
+        legend = Legend(component=plot, plots=plots,
+                        align="ur", border_padding=10)
+        legend.tools.append(LegendTool(legend, drag_button="left"))
+        legend.padding_right = -115
+        plot.overlays.append(legend)
+
+        container = VPlotContainer(width=plot.width + 100, halign='left')
+        plot.padding_bottom = 50
+        plot.padding_top = 10
+        plot.padding_left = 0
+        container.add(plot)
+
+        return container
+
+
+    #### View definition #####################################################
+
+    resizable_plot_item = Item(
+        'theta_plot',
+        editor=ComponentEditor(),
+        resizable=True,
+        show_label=False,
+        width=600,
+        height=400
+        )
+
+    traits_plot_item = Item(
+        'theta_plot',
+        editor=ComponentEditor(),
+        resizable=False,
+        show_label=False,
+        height=-250,
+        )
--- pyanno/ui/model_b_view.py
+++ pyanno/ui/model_b_view.py
@@ -1,33 +1,22 @@
 # Copyright (c) 2011, Enthought, Ltd.
 # Author: Pietro Berkes <pberkes@enthought.com>
 # License: Modified BSD license (2-clause)
-from chaco.array_plot_data import ArrayPlotData
-from chaco.data_range_2d import DataRange2D
-from chaco.label_axis import LabelAxis
-from chaco.legend import Legend
-from chaco.plot import Plot
-from chaco.plot_containers import OverlayPlotContainer, HPlotContainer
-from chaco.scales.scales import FixedScale
-from chaco.scales_tick_generator import ScalesTickGenerator
-from chaco.tools.legend_tool import LegendTool
-from chaco.default_colors import palette11 as COLOR_PALETTE
-
 from enable.component_editor import ComponentEditor
 
 from traits.has_traits import on_trait_change, HasTraits
 from traits.trait_numeric import Array
-from traits.trait_types import Instance, Str, Range, Button, Int, Any
+from traits.trait_types import Instance, Str, Range, Button, Int, Any, Enum
 from traitsui.editors.range_editor import RangeEditor
-from traitsui.group import VGroup, VGrid
+from traitsui.group import VGroup, VGrid, HGroup, Group
 from traitsui.include import Include
-from traitsui.item import Item
+from traitsui.item import Item, Spring
 from traitsui.menu import OKButton
 from traitsui.view import View
 
 from pyanno.modelB import ModelB
 from pyanno.plots.hinton_plot import HintonDiagramPlot
 from pyanno.plots.matrix_plot import MatrixPlot
-from pyanno.plots.plots_superclass import PyannoPlotContainer
+from pyanno.plots.theta_tensor_plot import ThetaTensorPlot
 from pyanno.ui.model_view import PyannoModelView, NewModelDialog
 from pyanno.ui.parameters_tabular_viewer import ParametersTabularView
 
@@ -93,12 +82,12 @@ class ModelB_MultipleThetaView(HasTraits):
                                         ),
                 ),
                 VGroup(
-                    Item('theta_j_view', style='custom', show_label=False)
+                    Item('theta_j_view',
+                         style='custom',
+                         show_label=False)
                 )
             ),
-            width = 500,
-            height = 400,
-            resizable = True
+            resizable=False
         )
         return traits_view
 
@@ -134,160 +123,6 @@ class ModelB_MatrixThetaView(ModelB_MultipleThetaView):
         self.theta_j_view = self._create_matrix_plot(self.annotator_idx)
 
 
-def sigmoid(x):
-    return 1./(1.+np.exp(-x))
-
-
-class LineThetaPlot(PyannoPlotContainer):
-
-    theta = Array
-
-    theta_samples = Any
-
-    annotator_idx = Int
-
-    theta_plot = Any
-
-    def _label_name(self, k):
-        return 'theta[{},{},:]'.format(self.annotator_idx,k)
-
-    def _theta_plot_default(self):
-
-        theta = self.theta
-        nclasses = theta.shape[0]
-
-        # create a plot data object and give it this data
-        plot_data = ArrayPlotData()
-
-        plot_data.set_data('classes', range(nclasses))
-
-        # create the plot
-        plot = Plot(plot_data, fill_padding=True)
-
-        # --- plot theta samples
-
-        if self.theta_samples is not None:
-            print 'got samples!!!'
-            nsamples = self.theta_samples.shape[0]
-            for k in range(nclasses):
-                samples = np.sort(self.theta_samples[:,k,:], axis=0)
-                perc5 = samples[int(nsamples*0.05),:]
-                perc95 = samples[int(nsamples*0.95),:]
-                avg = samples.mean(0)
-
-                # build polygon
-                index_name = self._label_name(k) + '_confint_index'
-                value_name = self._label_name(k) + '_confint_value'
-                index_coord = []
-                value_coord = []
-                # bottom part
-                for i in range(nclasses):
-                    index_coord.append(i)
-                    value_coord.append(perc5[i])
-                # top part
-                for i in range(nclasses-1, -1, -1):
-                    index_coord.append(i)
-                    value_coord.append(perc95[i])
-
-                plot_data.set_data(index_name, np.array(index_coord,
-                                                        dtype=float))
-                plot_data.set_data(value_name, np.array(value_coord,
-                                                        dtype=float))
-
-                # make color lighter and more transparent
-                color = list(COLOR_PALETTE[k % len(COLOR_PALETTE)])
-                for i in range(3):
-                    color[i] = min(1.0, sigmoid(color[i]*5.))
-                color[-1] = 0.3
-
-                plot.plot(
-                    (index_name, value_name),
-                    type = 'polygon',
-                    face_color = color,
-                    edge_color = 'black',
-                    edge_width = 0.5
-                )
-
-                # add average
-                avg_name = self._label_name(k) + '_avg_value'
-                plot_data.set_data(avg_name, avg)
-                print avg_name, avg
-                plot.plot(
-                    ('classes', avg_name),
-                    color = COLOR_PALETTE[k % len(COLOR_PALETTE)],
-                    line_style = 'dash'
-                )
-
-        # --- plot values of theta
-
-        data_names = ['classes']
-        for k in range(nclasses):
-            name = self._label_name(k)
-            plot_data.set_data(name, theta[k,:])
-            data_names.append(name)
-
-        plots = {}
-        for k in range(nclasses):
-            name = self._label_name(k)
-            line_plot = plot.plot(
-                ['classes', name],
-                line_width=2.,
-                color=COLOR_PALETTE[k % len(COLOR_PALETTE)],
-                name=name
-            )
-            plots[name] = line_plot
-
-
-        # --- adjust plot appearance
-
-        # adjust axis bounds
-        plot.range2d = DataRange2D(
-            low  = (-0.2, 0.0),
-            high = (nclasses-1+0.2, theta.max()*1.1)
-        )
-
-        # create new horizontal axis
-        label_axis = self._create_increment_one_axis(
-            plot, 0., nclasses, 'bottom')
-        label_axis.title = 'True classes'
-        self._add_index_axis(plot, label_axis)
-
-        # label vertical axis
-        plot.value_axis.title = 'Probability'
-
-        # use a FixedScale tick generator with a resolution of 1
-        label_axis.tick_generator = ScalesTickGenerator(scale=FixedScale(1.))
-
-        # add legend
-        legend = Legend(component=plot, plots=plots, padding=5,
-                        align="lr", border_padding=10)
-        legend.tools.append(LegendTool(legend, drag_button="left"))
-        plot.overlays.append(legend)
-
-        return plot
-
-
-    #### View definition #####################################################
-
-    resizable_plot_item = Item(
-        'theta_plot',
-        editor=ComponentEditor(),
-        resizable=True,
-        show_label=False,
-        width=600,
-        height=400
-        )
-
-    traits_plot_item = Item(
-        'theta_plot',
-        editor=ComponentEditor(),
-        resizable=False,
-        show_label=False,
-        height=-300,
-        width=-400
-        )
-
-
 class ModelB_LineThetaView(ModelB_MultipleThetaView):
     """Theta view is a set of line plot."""
 
@@ -295,7 +130,7 @@ class ModelB_LineThetaView(ModelB_MultipleThetaView):
         samples = None
         if self.theta_samples is not None:
             samples = self.theta_samples[:,annotator_idx,:,:]
-        plot_view = LineThetaPlot(
+        plot_view = ThetaTensorPlot(
             theta = self.theta[annotator_idx,:,:],
             annotator_idx = annotator_idx,
             theta_samples = samples
@@ -336,12 +171,15 @@ class ModelBView(PyannoModelView):
 
         self.theta_line_plot = ModelB_LineThetaView(theta=self.model.theta)
 
+        self._theta_view_update()
+
 
     def plot_theta_samples(self, theta_samples):
         self.theta_line_plot = ModelB_LineThetaView(
             theta = self.model.theta,
             theta_samples = theta_samples
         )
+        self._theta_view_update()
 
 
     #### UI traits
@@ -351,6 +189,20 @@ class ModelBView(PyannoModelView):
     theta_matrix_plot = Instance(ModelB_MatrixThetaView)
 
     theta_line_plot = Instance(ModelB_LineThetaView)
+
+    theta_views = Enum('Line plot (supports samples)', 'Matrix plot')
+
+    theta_view = Instance(ModelB_MultipleThetaView)
+
+    def _theta_view_default(self):
+        return self.theta_line_plot
+
+    @on_trait_change('theta_views')
+    def _theta_view_update(self):
+        if self.theta_views.startswith('Line'):
+            self.theta_view = self.theta_line_plot
+        else:
+            self.theta_view = self.theta_matrix_plot
 
 
     #### Actions
@@ -382,10 +234,15 @@ class ModelBView(PyannoModelView):
              resizable=False,
              show_label=False),
         Item('handler.view_pi', show_label=False),
-        Item('handler.theta_line_plot',
-             style='custom',
-             resizable=False,
-             show_label=False),
+        Item('_'),
+        Group(
+            Item('handler.theta_views',
+                 show_label=False),
+            Item('handler.theta_view',
+                 style='custom',
+                 resizable=False,
+                 show_label=False),
+        ),
         Item('handler.view_theta', show_label=False),
     )
 
