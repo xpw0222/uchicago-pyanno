cd647c19128c2a8c2331074b66c4bda9917849be
Pietro Berkes <pberkes@enthought.com>
2011-11-17 10:58:21 +0000

    FIX+DOC: remove unnecessary sphinxtogithub dependency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,8 +27,7 @@ sys.path.insert(0, os.path.abspath('./sphinxext'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode']
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
-              'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'refactor_doc',
-              'sphinxtogithub']
+              'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'refactor_doc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
